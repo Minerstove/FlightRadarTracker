cdb509b22d2e9e47275200f5a5bae198a795da08
Minerstove <rfgviterbo@gmail.com>
2024-07-08 19:58:34 +0800

    Created new function to log flights searched by ICAO

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const { FlightRadar24API } = require("flightradarapi");
 const frApi = new FlightRadar24API();
 const fs = require('fs'); //To write to a file
+const prompt = require("prompt-sync")();
+let value = prompt("What airline do you want to find flights? ");
 
 //let flights = frApi.getFlights(...);
 async function airlineList(){
@@ -18,23 +20,33 @@ async function airlineList(){
     return 0;
 }
 
-async function flightsearcherByICAO(){
-    const writeStream = fs.createWriteStream('flights.txt');
-    let flights = await frApi.getFlights("PAL");
+async function flightsearcherByICAO(ICAOcode){
+    let flights = await frApi.getFlights(ICAOcode);
     console.log(flights);
-    flights.forEach(value => writeStream.write(`${JSON.stringify(value)}\n`));
-    writeStream.on('finish', ()=> {
-        console.log('wrote all the array data to file Airlines.txt');
-    });
-    writeStream.on('error', (err) => {
-        console.error(`There is an error writing the file Airlines.txt => ${err}`)
-    });
-    writeStream.end();
     console.log(flights[1].latitude)
     return 0;
 }
+flightsearcherByICAO(value);
 
-flightsearcherByICAO();
+/*document.addEventListener('DOMContentLoaded', (event) => {
+    var inputElement = document.getElementById('myInput');
+    const buttonElement = document.getElementById('myButton');
+
+    // Run a function when the button is clicked
+    buttonElement.addEventListener('click', function() {
+        runOnButtonClick(inputElement.value);
+    });
+});
+
+function runOnButtonClick(value) {
+    console.log('Input value is: ' + value);
+    
+    // Your code here
+}
+*/
+
+
+
 //airlineList();
 
     //fs.writeFile('Airlines.txt',airlines,(err)=>{if (err) throw err;}) data cannot be an array therefore
